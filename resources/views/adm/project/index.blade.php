9602866856770c9aaef95092055e352ac761debf
Ian Travers <mr.itpe@gmail.com>
2019-02-27 13:52:04 +0300

    Added legend to projects index adm

--- resources/views/adm/project/index.blade.php
+++ resources/views/adm/project/index.blade.php
@@ -11,6 +11,11 @@
             <a href="{{ route('adm.projects.create') }}" class="btn btn-outline-primary">
                 Добавить проект
             </a>
+            <div class="d-inline-flex float-right mt-2">
+                <span class="bg-primary px-4 mr-2"></span> &mdash; <span class="px-2">просрочен</span>
+                <span class="bg-danger px-4 mr-2"></span> &mdash; <span class="px-2">последний день</span>
+                <span class="bg-warning px-4 mr-2"></span> &mdash; <span class="px-2">осталось менее 2-х недель</span>
+            </div>
         </div>
         <div class="card-body">
 
