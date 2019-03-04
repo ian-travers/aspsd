55296e381f6ea50a34f6d11a4cf590673bfb200b
Ian Travers <mr.itpe@gmail.com>
2019-03-04 13:04:13 +0300

    Rename the legend item

--- resources/views/projector/project/index.blade.php
+++ resources/views/projector/project/index.blade.php
@@ -12,7 +12,7 @@
                 Добавить проект
             </a>
             <div class="d-inline-flex float-right mt-2">
-                <span class="mr-2 text-success font-weight-bold">&#10004;</span> &mdash; <span class="px-2">подтверждено получение</span>
+                <span class="mr-2 text-success font-weight-bold">&#10004;</span> &mdash; <span class="px-2">подтверждено</span>
                 <span class="bg-primary px-4 mr-2"></span> &mdash; <span class="px-2">просрочен</span>
                 <span class="bg-danger px-4 mr-2"></span> &mdash; <span class="px-2">последний день</span>
                 <span class="bg-warning px-4 mr-2"></span> &mdash; <span class="px-2">осталось менее 2-х недель</span>
