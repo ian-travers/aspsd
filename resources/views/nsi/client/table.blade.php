d389162c0b619fa09f1c4cc672714f652b71e5ea
Ian Travers <mr.itpe@gmail.com>
2019-04-05 09:01:27 +0300

    Use font-awesome icons in the action column (nsi)

--- resources/views/nsi/client/table.blade.php
+++ resources/views/nsi/client/table.blade.php
@@ -1,39 +1,30 @@
 <table class="table table-bordered">
     <thead>
     <tr class="bg-light">
-        <td width="178" class="text-center">Действия</td>
+        <td width="10%" class="text-center">Действия</td>
         <td>Наименование</td>
     </tr>
     </thead>
     <tbody>
 
-    @php
-        /* @var App\Client $client */
-
-    @endphp
+    @php /* @var App\Client $client */ @endphp
 
     @foreach($clients as $client)
 
         <tr>
             <td class="text-center">
 
-                <a href="{{ route('nsi.clients.edit', $client->id) }}" class="btn btn-outline-secondary btn-sm">
-                    Изменить
-                </a>
+                <a href="{{ route('nsi.clients.edit', $client->id) }}" class="btn btn-outline-secondary btn-sm fa fa-pen" title="Изменить"></a>
 
                 @if($client->isDeletable())
                 <form class="d-inline" action="{{ route('nsi.clients.destroy', $client) }}" method="post">
 
                     @method('delete')
                     @csrf
-                    <button type="submit" onclick="return confirm('Подтверждаете удаление?')" class="btn btn-outline-danger btn-sm">
-                        Удалить
-                    </button>
+                    <button type="submit" onclick="return confirm('Подтверждаете удаление?')" class="btn btn-outline-danger btn-sm fa fa-trash" title="Удалить"></button>
                 </form>
                 @else
-                    <button type="button" class="btn btn-outline-danger btn-sm disabled">
-                        Удалить
-                    </button>
+                    <button type="button" class="btn btn-outline-danger btn-sm disabled fa fa-trash"></button>
                 @endif
 
 
